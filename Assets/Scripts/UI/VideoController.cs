d6f190e83698ad46c3e14cd8fee589026789202b
Renan <renanclaudino@gmail.com>
2020-02-11 20:56:36 -0300

    Reload button (video controller)

--- Assets/Scripts/UI/VideoController.cs
+++ Assets/Scripts/UI/VideoController.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 namespace Tecktal
 {
-
+    [RequireComponent(typeof(VideoPlayer))]
     public class VideoController : MonoBehaviour
     {
-        
+
+        VideoPlayer videoPlayer;
+
+        private void Start()
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+        }
+
         public void Home()
         {
             Destroy(gameObject);
+        }
+
+        public void Reload()
+        {
+            videoPlayer.Stop();
+            StartCoroutine(IPlay());
+        }
+
+        IEnumerator IPlay()
+        {
+            yield return new WaitForSeconds(0.1f);
+            videoPlayer.Play();
         }
     }
 }
