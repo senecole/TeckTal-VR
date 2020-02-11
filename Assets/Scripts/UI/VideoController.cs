40311b270e119867c9dc28ddb8857fdebe8866c1
Renan <renanclaudino@gmail.com>
2020-02-11 20:46:17 -0300

    home button (video controller)

---
+++ Assets/Scripts/UI/VideoController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tecktal
+{
+
+    public class VideoController : MonoBehaviour
+    {
+        
+        public void Home()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
